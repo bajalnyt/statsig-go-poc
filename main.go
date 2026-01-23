76c04d9877cf0c2e87bbe52daa5105978bce55ea
Bajal <mohiyudeen.bajal@nytimes.com>
2026-01-23 14:42:33 -0500

    init

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	statsig.InitializeWithOptions(
 
+		"supply-secret-here",
 		&statsig.Options{
 			Environment: statsig.Environment{Tier: "production"},
 		},
@@ -20,7 +21,7 @@ func main() {
 	// check 10 random users
 	for i := 0; i < 100; i++ {
 		userID := rand.Int64()
-		stat := IsActive("flag_poc", userID)
+		stat := IsActive("payflow_apple_pay_via_pug", userID)
 		fmt.Printf("User %d: %t\n", userID, stat)
 		if stat {
 			countActive++
@@ -33,10 +34,7 @@ func main() {
 
 func IsActive(featureName string, regi int64) bool {
 	user := statsig.User{CustomIDs: map[string]string{"regi_id": fmt.Sprint(regi)}}
-	// If this is a gate, we need to check if the user is in the gate
 	return statsig.CheckGate(user, featureName)
-
-	// If this is an experiment, we need to get the treatment
-	//experiment := statsig.GetExperiment(user, featureName)
-	//return experiment.GetBool(featureName, true)
+	experiment := statsig.GetExperiment(user, featureName)
+	return experiment.GetBool(featureName, true)
 }
