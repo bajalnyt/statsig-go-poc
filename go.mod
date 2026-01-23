c3364a15a2e917a151772f1d7244fed5a9a4832d
Bajal <mohiyudeen.bajal@nytimes.com>
2026-01-23 14:25:47 -0500

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module bajalnyt/statsig-go-poc
+
+go 1.24.6
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
+	github.com/statsig-io/go-sdk v1.40.1 // indirect
+	github.com/statsig-io/ip3country-go v0.3.0 // indirect
+	github.com/ua-parser/uap-go v0.0.0-20250917011043-9c86a9b0f8f0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+
+	statsig "github.com/statsig-io/go-sdk"
+)
+
+func main() {
+	statsig.InitializeWithOptions(
+
+		&statsig.Options{
+			Environment: statsig.Environment{Tier: "production"},
+		},
+	)
+
+	countActive := 0
+	countInactive := 0
+	// check 10 random users
+	for i := 0; i < 100; i++ {
+		userID := rand.Int64()
+		stat := IsActive("my_flag_name", userID)
+		fmt.Printf("User %d: %t\n", userID, stat)
+		if stat {
+			countActive++
+		} else {
+			countInactive++
+		}
+	}
+	fmt.Printf("Count Active: %d, Count Inactive: %d\n", countActive, countInactive)
+}
+
+func IsActive(featureName string, regi int64) bool {
+	user := statsig.User{CustomIDs: map[string]string{"regi_id": fmt.Sprint(regi)}}
+	return statsig.CheckGate(user, featureName)
+}
